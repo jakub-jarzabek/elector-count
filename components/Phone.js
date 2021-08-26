8e55e86461dc8b1d3589c234537cdae8ccfb717b
Jakub Jarzabek <kubitssu@gmail.com>
2021-08-26 19:02:10 +0200

    created phone component

--- components/Phone.js
+++ components/Phone.js
@@ -4,9 +4,11 @@ import styles from '../styles/Phone.module.scss'
 
 export default function Phone() {
   return (
-    <div className={styles.PhoneWrapper}>
-      <div className={styles.PhoneCase}>
-        <div className={styles.PhoneScreen}></div>
+    <div className={styles.PhoneCase}>
+      <div className={styles.PhoneScreen}></div>
+      <div className={styles.PhoneNotch}>
+        <div className={styles.Speaker} />
+        <div className={styles.Camera} />
       </div>
     </div>
   )
