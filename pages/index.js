d0f3bcbf087b25adfa95cf60c2b42d8562ac5346
Jakub Jarzabek <kubitssu@gmail.com>
2021-08-26 22:21:15 +0200

    added sign in btn

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import React from "react";
-import Link from "next/link";
-import {signIn, signOut, useSession} from "next-auth/client";
+import React from 'react'
+import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import Phone from '../components/Phone'
 
 export default function Home() {
-    const [session, loading] = useSession();
+  const [session, loading] = useSession()
   return (
-
     <>
-        {!session && (
-            <>
-                Not signed in
-                <button onClick={signIn}>Sign In</button>
-            </>
-        )}
-        {session && (
-            <Phone />
-        )}
-
+      {!session && (
+        <>
+          <Phone>
+            <button onClick={signIn} className={styles.SignInBtn}>
+              Sign In
+            </button>
+          </Phone>
+        </>
+      )}
+      {session && <Phone />}
     </>
   )
 }
