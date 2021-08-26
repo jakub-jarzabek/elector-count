d0b5d803f588052d32226efdb8541fc5e1a0eee6
Kacper <kacper.reja@gmail.com>
2021-08-26 22:22:36 +0200

    index

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import React from "react";
 import Link from "next/link";
-import {signIn, signOut, useSession} from "next-auth/client";
+
 
 import Phone from '../components/Phone'
 
